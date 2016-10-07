648b15c35f4b42b530b4d875b3beef4ee5a1cb0c
A K <A K>
2016-10-07 20:57:28 +0100

    Added commens list, commens and props

--- lesson2/src/index.js
+++ lesson2/src/index.js
@@ -1,12 +1,51 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var ComponentExample = React.createClass({
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var ComponentBox = React.createClass({
     render: function() {
         return (
-            <div className="commentBox">Hello, world! I am a CommentBox.</div>
-        );
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+                </div>
+                );
     }
 });
 
-ReactDOM.render(React.createElement(ComponentExample), document.getElementById('app')); 
+
+ReactDOM.render(<ComponentBox />, document.getElementById('app')); 
